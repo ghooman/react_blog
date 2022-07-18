c6b5074c3fb3b81a2cd79ff17e35a0d05d4d7a11
ghooman <ghoopark1@gmail.com>
2022-07-19 02:21:38 +0900

    글발행, 삭제

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import { useState } from "react";
 
 function App() {
 
-  let post = "강남 우동 맛집";
-  
   let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집", "js독학"]);
-
   let [따봉, 따봉변경] = useState([0, 0, 0]);
-
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -19,55 +17,54 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      {/* <div className="list">
-        <h4>
-          {글제목[0]} <span onClick={() => {따봉변경(따봉+1)}}>👍🏻</span> {따봉}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4 onClick={() => {setModal(true)}}>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div> */}
-
       {
         글제목.map((el, i) => {
           return (
             <div className="list" key={i}>
-              <h4>
+              <h4 onClick={() => {setModal(true); setTitle(i)}}>
                 {글제목[i]}
-                <span onClick={() => {
+                <span onClick={(e) => {
+                  e.stopPropagation();
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
                   따봉변경(copy)
                 }}>👍🏻</span> {따봉[i]}
               </h4>
               <p>2월 17일 발행</p>
+              <button onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}>삭제</button>
             </div>
           )
         })
       }
+
+      <input onChange={(e) => {
+        입력값변경(e.target.value)
+      }} />
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.unshift(입력값);
+        글제목변경(copy)
+      }}>글발행</button>
       
       { 
-        modal == true ? <Modal /> : null 
+        modal == true ? <Modal 글제목={글제목} title={title}/> : null 
       }
 
     </div>
   );
 }
 
-const Modal = () => {
+const Modal = (props) => {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
     </div>
   )
 }
